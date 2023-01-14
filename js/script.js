c9eddfda4e8ba0c6dd738b4987a8903412282cc2
Pedro/OrdenadorTorre <pedri397@gmail.com>
2023-01-14 18:33:31 +0100

    Diseño General

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const api = {
+    key: '6fa9bf538b033b05cb13df8cf57db921',
+    url: `https://api.openweathermap.org/data/2.5/weather`
+}
+
+let form__search = document.getElementById("form__search")
+let search__txt = document.getElementById("search__txt")
+let btsearch = document.getElementById("btsearch")
